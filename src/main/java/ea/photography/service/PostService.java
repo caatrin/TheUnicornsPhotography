164d7824e67f5cce8ddced1bb995be0e11c32cc5
jorgevalbuena56 <jorgevalbuena56@gmail.com>
2015-04-20 21:49:37 -0500

    Implementation of the Creation and List Post

--- src/main/java/ea/photography/service/PostService.java
+++ src/main/java/ea/photography/service/PostService.java
@@ -5,10 +5,34 @@
  */
 package ea.photography.service;
 
+import ea.photography.dao.PostDao;
+import ea.photography.domain.Post;
+import java.util.List;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  *
  * @author caatrin
  */
+@Transactional(propagation = Propagation.REQUIRES_NEW)
 public class PostService {
+    
+    private PostDao postDao;
+
+    public PostService() {
+    }
+
+    public void setPostDao(PostDao postDao) {
+        this.postDao = postDao;
+    }
+    
+    public void addNewPost(Post post){
+        postDao.add(post);
+    }
+    
+    public List<Post> getAllPost(){
+        return postDao.getAll();
+    }
     
 }
---
+++ userAdmin.sql
@@ -0,0 +1,3 @@
+use photography;
+INSERT INTO user VALUES(1, "123", "admin@gmail.com","Maharishi","Yogi Bear", "123", "ADMIN");
+commit;
